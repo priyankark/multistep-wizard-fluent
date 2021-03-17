aa799d3280842cd8351620f8ca9c215abdcc587f
priykum <priykum@microsoft.com>
2021-03-18 00:06:32 +0530

    Footer snapshot test

---
+++ src/components/__tests__/Footer.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import {Footer} from '../Footer'; 
+import {render} from '@testing-library/react';
+
+describe('Footer tests', ()=>{
+    test('basic snapshot', ()=>{
+        const {asFragment} = render(<Footer/>); 
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
