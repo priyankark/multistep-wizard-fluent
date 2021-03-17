fbd037a4cb0884aeb19e985e9f3c70bd7170b8a1
priykum <priykum@microsoft.com>
2021-03-18 00:06:20 +0530

    Unit test setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // if you're also using typescript
+    preset: "ts-jest",
+    testEnvironment: "node",
+    verbose: true,
+    // registers babel.config.js with jest
+    transform: {
+      "^.+\\.js$": "babel-jest",
+    },
+    "moduleNameMapper": {
+      "@fluentui/react/lib/(.*)$": "@fluentui/react/lib-commonjs/$1"
+    },
+    // explicitly include any node libs using ESM modules
+    transformIgnorePatterns: ["node_modules/?!(@fluentui)"],
+  }
